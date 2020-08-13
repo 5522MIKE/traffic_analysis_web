9129f6d72ced6d5ea4c75de85693de1da2318ec4
MikyMing <18988774300@163.com>
2020-08-13 16:31:46 +0800

    feat：视频播放组件

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -24,7 +24,7 @@
             <Layout>
                 <Layout>
                     <Content :style="{margin: '20px 20px 0', background: '#dcdee2', maxWidth: '1024px'}">
-                        <video
+                        <!-- <video
                             id="myVideo"
                             class="video-js"
                             >
@@ -32,7 +32,8 @@
                                 src="../assets/1.mp4"
                                 type="video/mp4"
                             >
-                        </video>
+                        </video> -->
+                        <VideoPlay/>
                     </Content>
                     <Sider :style="{margin: '20px 20px 0', background: '#dcdee2',minHeith: '580px', minWidth: '500px' , background: '#e8eaec'}">
                             <p v-if='vIf == 1'><vehicle/></p>
@@ -56,11 +57,13 @@
 <script>
 import number from '@/components/number'
 import vehicle from '@/components/vehicle'
+import VideoPlay from '@/components/VideoPlay'
 /* eslint-disable */
 export default {
     components: {
         number,
-        vehicle
+        vehicle,
+        VideoPlay
     },
     data() {
         return {
@@ -133,29 +136,7 @@ export default {
                     ],
         };
     },
-    mounted() { 
-        this.initVideo();
-    },
     methods: {
-        initVideo() {
-        //初始化视频方法
-        let myPlayer = this.$video(myVideo, {
-            //确定播放器是否具有用户可以与之交互的控件。没有控件，启动视频播放的唯一方法是使用autoplay属性或通过Player API。
-            controls: true,
-            //自动播放属性,muted:静音播放
-            // autoplay: "muted",
-            autoplay: "false",
-            //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
-            preload: "auto",
-            //设置视频播放器的显示宽度（以像素为单位）
-            width: "1024px",
-            //设置视频播放器的显示高度（以像素为单位）
-            height: "588px",
-            //可选择的播放速度
-            playbackRates : [ 0.5, 1.0, 1.5, 2.0 ], 
-            notSupportedMessage : '此视频暂无法播放，请稍后再试'
-        });
-        },
         getUrl(name){
             this.showhone = false;
             console.log(name);
--- src/components/VideoPlay.vue
+++ src/components/VideoPlay.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="test_two_box">
-    <video id="myVideo" class="video-js" :src="videoSrc" type="video/mp4">
-    <!-- <source
-        src="../assets/1.mp4"
+    <video id="myVideo" class="video-js">
+    <source
+        :src="videoSrc"
         type="video/mp4"
-    > -->
+    >
     </video>
 </div>
 </template>
@@ -35,9 +35,9 @@ methods: {
         //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
         preload: "auto",
         //设置视频播放器的显示宽度（以像素为单位）
-        width: "800px",
+        width: "600px",
         //设置视频播放器的显示高度（以像素为单位）
-        height: "400px",
+        height: "600px",
         //可选择的播放速度
         playbackRates : [ 0.5, 1.0, 1.5, 2.0 ], 
         notSupportedMessage : '此视频暂无法播放，请稍后再试'
